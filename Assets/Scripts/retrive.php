9d6f30cbc59d464bb6f9de2dde3f4393eb5f1a60
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-01-26 01:28:45 +0530

    Upgraded and most stuff works

--- Assets/Scripts/retrive.php
+++ Assets/Scripts/retrive.php
@@ -2,18 +2,18 @@
 
 include 'connection.php';
 
-	$sql = "SELECT * FROM `user_road` ORDER BY 'score' ";
+	$sql = "SELECT * FROM `user` ORDER BY 'score' ";
 
 	$result = $con->query($sql);
 
 	if ($result->num_rows <= 0) {
-		echo "1";
+		echo "0";
 	}
 	else{
-		echo "2";
+		echo $result->num_rows;
 	}
 	while ($col = $result->fetch_assoc()){
-		echo "+" . $col['name'] . "+" . $col['score'] ;
+		echo "+" . $col['Name'] . "+" . $col['Score'] ;
 	}
 
 ?>

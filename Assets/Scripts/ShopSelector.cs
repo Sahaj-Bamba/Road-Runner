73bc5eefc1af472087245fe7096786d269923021
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-02-05 01:58:55 +0530

    added shop feedback and credits and settings

---
+++ Assets/Scripts/ShopSelector.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ShopSelector : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public GameObject levelIcon;
+    public GameObject thisCanvas;
+    public int numberOfLevels = 50;
+    public Vector2 iconSpacing;
+    private Rect panelDimensions;
+    private Rect iconDimensions;
+    private int amountPerPage;
+    private int currentLevelCount;
+    public string []A;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
+        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        int maxInARow = Mathf.FloorToInt((panelDimensions.width + iconSpacing.x) / (iconDimensions.width + iconSpacing.x));
+        int maxInACol = Mathf.FloorToInt((panelDimensions.height + iconSpacing.y) / (iconDimensions.height + iconSpacing.y));
+        amountPerPage = maxInARow * maxInACol;
+        int totalPages = Mathf.CeilToInt((float)numberOfLevels / amountPerPage);
+        LoadPanels(totalPages);
+    }
+    void LoadPanels(int numberOfPanels)
+    {
+        GameObject panelClone = Instantiate(levelHolder) as GameObject;
+        PageSwiper swiper = levelHolder.AddComponent<PageSwiper>();
+        swiper.totalPages = numberOfPanels;
+
+        for (int i = 1; i <= numberOfPanels; i++)
+        {
+            GameObject panel = Instantiate(panelClone) as GameObject;
+            panel.transform.SetParent(thisCanvas.transform, false);
+            panel.transform.SetParent(levelHolder.transform);
+            panel.name = "Page-" + i;
+            panel.GetComponent<RectTransform>().localPosition = new Vector2(panelDimensions.width * (i - 1), 0);
+            SetUpGrid(panel);
+            int numberOfIcons = i == numberOfPanels ? numberOfLevels - currentLevelCount : amountPerPage;
+            LoadIcons(numberOfIcons, panel);
+        }
+        Destroy(panelClone);
+    }
+    void SetUpGrid(GameObject panel)
+    {
+        GridLayoutGroup grid = panel.AddComponent<GridLayoutGroup>();
+        grid.cellSize = new Vector2(iconDimensions.width, iconDimensions.height);
+        grid.childAlignment = TextAnchor.MiddleCenter;
+        grid.spacing = iconSpacing;
+    }
+    void LoadIcons(int numberOfIcons, GameObject parentObject)
+    {
+        for (int i = 1; i <= numberOfIcons; i++)
+        {
+            currentLevelCount++;
+            GameObject icon = Instantiate(levelIcon) as GameObject;
+            icon.transform.SetParent(thisCanvas.transform, false);
+            icon.transform.SetParent(parentObject.transform);
+            icon.name = A[i-1];
+            icon.GetComponentInChildren<Text>().text = A[i-1];
+            icon.GetComponentInChildren<Button>().onClick.AddListener(() => load(icon.GetComponentInChildren<Text>().text));
+        }
+    }
+
+    void load(string x)
+    {
+        PlayerPrefs.SetString("special", x);
+        Debug.Log(x);
+        SceneManager.LoadScene("LevelSelector");
+    }
+
+    
+}
